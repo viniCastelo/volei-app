f0fd6204978e753b9315237dacf6e176a670bf26
Vinícius Castelo <vkstello@gmail.com>
2023-03-19 05:40:27 -0300

    Removendo pacote sem utilização

--- lib/controllers/settings_controller.dart
+++ lib/controllers/settings_controller.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:volei/controllers/home_controller.dart';
 import 'package:volei/util/global/global.dart';
 
 class SettingsController extends GetxController with Global {
